fea1bf21018b4eca9a27df3625fe4628d14c0e33
Jhessica Kanda <jhessica@ebanx.com>
2018-02-20 15:39:51 -0300

    Basics

---
+++ basics/module.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  console.log('Function was called');
+}
+
+var myString = 'My String';
+
+module.exports.myFunction = myFunction;
+module.exports.myString = myString;
--- basics/server.js
+++ basics/server.js
@@ -1,8 +1,13 @@
 var http = require('http');
+var module = require('./module');
+var module2 = require('./module2');
 
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
+    response.write(module.myString);
+    response.write(module2.myString);
+    module.myFunction();
+    module2.myFunction();
     response.end();
 }
 
